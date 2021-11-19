c7aeeb96fdc393bf5b4f1d1077977f980f1ada9e
archy7seven <august.ivan20@gmail.com>
2021-11-19 13:11:09 +0700

    clear home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,11 +31,11 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-// $routes->get('/', 'Home::index');
-// $routes->get('/login', 'Login::index');
-// $routes->post('/operator/auth', 'Operator::auth');
-// $routes->post('/operator/register', 'Operator::register');
-// $routes->get('/logout', 'Login::logout');
+$routes->get('/', 'Home::index');
+$routes->get('/login', 'Login::index');
+$routes->post('/operator/auth', 'Operator::auth');
+$routes->post('/operator/register', 'Operator::register');
+$routes->get('/logout', 'Login::logout');
 
 // Route Home
 $routes->get('/home/(:any)', 'Home::index');
