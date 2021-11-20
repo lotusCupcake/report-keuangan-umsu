38257a597becccc70350d5420f889fc56e882857
fikriansari <fikriansari.mfa@gmail.com>
2021-11-20 11:21:39 +0700

    fix bug

--- app/Views/pages/tunggakan.php
+++ app/Views/pages/tunggakan.php
@@ -39,11 +39,11 @@
                                     <option>Tunggakan Tahap</option>
                                     <option value="1">Tahap 1</option>
                                 </select>
-                            </div>     
-                            <ul class="panel-controls" >
+                            </div>
+                            <ul class="panel-controls">
                                 <button type="submit" class="btn btn-success">Cari</button>
-                                <?php if ($termYear != null && $entryYear != null) :?>
-                                <a href="/tunggakan/" class="btn btn-info">Export</a>
+                                <?php if ($termYear != null && $entryYear != null) : ?>
+                                    <a href="/tunggakan/" class="btn btn-info">Export</a>
                                 <?php endif ?>
                             </ul>
                         </form>
@@ -64,25 +64,25 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php if (count($tunggakan)>0) :?>
-                                    <?php foreach ($tunggakan as $rows) :?>
-                                        <tr>
-                                            <td><?= $rows->NO_REGISTER ?></td>
-                                            <td><?= $rows->Npm ?></td>
-                                            <td><?= $rows->NAMA_LENGKAP ?></td>
-                                            <td><?= $rows->NAMA_PRODI ?></td>
-                                            <td><?= $rows->ANGKATAN ?></td>
-                                            <td><?= $rows->NAMA_BIAYA ?></td>
-                                            <td><?= $rows->TAHAP ?></td>
-                                            <td><?= number_to_currency($rows->NOMINAL, 'IDR') ?></td>
-                                        </tr>
-                                    <?php endforeach ?>
-                                    <?php else :?>
+                                    <?php if (count($tunggakan) > 0) : ?>
+                                        <?php foreach ($tunggakan as $rows) : ?>
+                                            <tr>
+                                                <td><?= $rows->NO_REGISTER ?></td>
+                                                <td><?= $rows->Npm ?></td>
+                                                <td><?= $rows->NAMA_LENGKAP ?></td>
+                                                <td><?= $rows->NAMA_PRODI ?></td>
+                                                <td><?= $rows->ANGKATAN ?></td>
+                                                <td><?= $rows->NAMA_BIAYA ?></td>
+                                                <td><?= $rows->TAHAP ?></td>
+                                                <td><?= number_to_currency($rows->NOMINAL, 'IDR') ?></td>
+                                            </tr>
+                                        <?php endforeach ?>
+                                    <?php else : ?>
                                         <tr>
                                             <td colspan=8 style="text-align:center">Tidak ada data</td>
                                         </tr>
                                     <?php endif ?>
-                                    
+
                                 </tbody>
                             </table>
                         </div>
