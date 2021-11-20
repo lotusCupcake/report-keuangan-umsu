a353558b42a0e0c5ef1ce83d0856030e3454105b
archy7seven <august.ivan20@gmail.com>
2021-11-20 11:16:56 +0700

    fix logic cetak tunggakan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -67,7 +67,7 @@ $routes->add('/endorsement/(:num)/edit', 'Endorsement::edit/$1');
 
 // Route  Tunggakan
 $routes->get('/tunggakan/(:any)', 'Tunggakan::index');
-$routes->get('/tunggakan/(:any)/(:any)', 'Tunggakan::cetakTunggakan/$1/$2');
+$routes->get('/tunggakan/(:num)/(:num)', 'Tunggakan::cetakTunggakan/$1/$2');
 $routes->post('/tunggakan', 'Tunggakan::prosesTunggakan');
 $routes->get('/test', 'Tunggakan::test');
 
