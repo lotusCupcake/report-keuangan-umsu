1ad7976d7a311f5773781c992aff1e656390e5bc
archy7seven <august.ivan20@gmail.com>
2021-11-23 16:09:40 +0700

    proses laporan pembayaran

--- app/Views/layout/templateSidebar.php
+++ app/Views/layout/templateSidebar.php
@@ -24,16 +24,16 @@
         <li>
             <a href="/home"><span class="fa fa-home"></span><span class="xn-text">Dashboard</span></a>
         </li>
-        <li class="xn-title">Jenis Laporan</li>
+        <li class="xn-title">Laporan</li>
         <li>
-            <a href="/tunggakan"><span class="fa fa-ban"></span><span class="xn-text">Tunggakan</span></a>
+            <a href="/tunggakan"><span class="fa fa-ban"></span><span class="xn-text">Lap. Detail Tunggakan</span></a>
         </li>
         <li>
-            <a href=" /pembayaran"><span class="fa fa-money"></span><span class="xn-text">Pembayaran</span></a>
+            <a href=" /pembayaran"><span class="fa fa-money"></span><span class="xn-text">Lap. Detail Pembayaran</span></a>
         </li>
         <li class="xn-title">Setting</li>
         <li>
-            <a href="/ubahTanggalTahap"><span class="fa fa-edit"></span><span class="xn-text">Ubah Tanggal Tahap</span></a>
+            <a href="/ubahTanggalTahap"><span class="fa fa-edit"></span><span class="xn-text">Set. Tgl. Tahap/Angkatan</span></a>
         </li>
     </ul>
     <!-- END X-NAVIGATION -->
