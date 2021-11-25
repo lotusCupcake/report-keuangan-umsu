1c3425876b0761deb0aeec5337070b4ca33ae73c
archy7seven <august.ivan20@gmail.com>
2021-11-25 12:04:55 +0700

    design export total tunggakan

--- app/Controllers/TunggakanTotal.php
+++ app/Controllers/TunggakanTotal.php
@@ -163,7 +163,6 @@ class TunggakanTotal extends BaseController
 
         $spreadsheet = new Spreadsheet();
         $col =   array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-        dd($col[2+(count($angkatan)-1)]);
         $row = 1;
         
         $spreadsheet->setActiveSheetIndex(0)->setCellValue('A' . $row, "Rekap Tunggakan")->mergeCells("A" . $row . ":" . $col[2+(count($angkatan)-1)] . $row)->getStyle("A" . $row . ":" . $col[2+(count($angkatan)-1)] . $row)->getFont()->setBold(true);
